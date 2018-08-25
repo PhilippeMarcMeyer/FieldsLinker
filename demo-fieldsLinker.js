e3db09ad8611b5218aa8fd52b1070ee717fef8bd
philippe MEYER <pmg.meyer@gmail.com>
2018-08-25 18:41:21 +0200

    * v 0.1 jquery plugIn fieldsLinker using canvas. the horizontal middle of each cell is automaticaly computed, the line is drawn while dragging and there's no need to give the list width

--- demo-fieldsLinker.js
+++ demo-fieldsLinker.js
@@ -6,10 +6,9 @@ var data = {};
 var canvasId = "";
 var canvasCtx = null;
 var canvasPtr = null;
-var List1Width = 300;
-var List2Width = 300;
-var canvasWidth = 300;
-var canvasHeight = 500;
+var canvasWidth = 0;
+var canvasHeight = 0;
+var canvasHalfWidth = 0;
 var onError = false;
 var className = "fieldsLinker";
 var byName = false;
@@ -17,9 +16,12 @@ var linksByOrder=[];
 var linksByName=[];
 var List1 = [];
 var List2 = [];	
-var move = {};
+var ListHeights1 = [];
+var ListHeights2 = [];	
+var move = null;
 var that = null;
-var cellHeight = 50;
+var lineStyle = "straight"; // straight or curve
+
 
 
 	
@@ -29,27 +31,26 @@ var draw = function(){
 	canvasCtx.strokeStyle = 'black';
 	canvasCtx.clearRect(0, 0, canvasWidth, canvasHeight);
 	canvasCtx.beginPath(); 
+	
 	linksByOrder.forEach(function(item,i){
-		
 		var _from = item["from"];
 		var _to = item["to"];
 		
 		var Ax = 0;
-		var Bx = canvasWidth;
+		var Ay = ListHeights1[_from];
 
-		var Ay = (_from * cellHeight) + cellHeight/2;
-		var By = (_to * cellHeight) + cellHeight/2;
+		var Bx = canvasWidth;
+		var By = ListHeights2[_to];
 		
 		canvasCtx.moveTo(Ax, Ay);
+
 		canvasCtx.lineTo(Bx, By);
 		canvasCtx.stroke();
-
 	});
 }
 	
 
 var makeLink  = function(infos){
-	
 	eraseLinkA(infos.offsetA);
 	linksByOrder.push({"from":infos.offsetA,"to":infos.offsetB});
 	linksByName.push({"from":infos.nameA,"to":infos.nameB});
@@ -103,22 +104,16 @@ $.fn.fieldsLinker = function(action,input) {
 			if(data.options.byName){
 				byName = data.options.byName;
 			}
-			if(data.options.cellHeight){
-				cellHeight = data.options.cellHeight;
-			}
-			if(data.options.canvasWidth){
-				canvasWidth = data.options.canvasWidth;
-			}
-			if(data.options.List1Width){
-				List1Width = data.options.List1Width;
-			}
-			if(data.options.List2Width){
-				List2Width = data.options.List2Width;
-			}	
 			
-		
+			if(data.options.lineStyle){
+				if(data.options.lineStyle=="curve")
+					lineStyle = "curve";
+			}
 			
+			
+
 			$(this).html("");
+			
 			var $main = $("<div></div>");
 			$main
 				.appendTo($(this))
@@ -129,20 +124,22 @@ $.fn.fieldsLinker = function(action,input) {
 			$leftDiv
 				.appendTo($main)
 				.addClass("FL-left")
-				.css({"position":"absolute","left":"0px","width":List1Width+"px","display":"inline-block","text-align":"left"})
+				.css({"float":"left","width":"40%","display":"inline-block","text-align":"left"})
 				.html(data.listA.name);
 
 			var $midDiv =  $("<div></div>");
 			$midDiv
 				.appendTo($main)
 				.addClass("FL-mid")
-				.css({"display":"inline-block","position":"absolute","left":List1Width+"px","width":canvasWidth+"px"});
-			
+				.css({"display":"inline-block","width":"20%"})
+				.html("&nbsp;");
+				
+				
 			var $rightDiv =  $("<div></div>");
 			$rightDiv
 				.appendTo($main)
 				.addClass("FL-right")
-				.css({"position":"absolute","left":(List1Width + canvasWidth)+"px","width":List2Width+"px","display":"inline-block","text-align":"left"})
+				.css({"float":"right","width":"40%","display":"inline-block","text-align":"left"})
 				.html(data.listB.name);
 				
 			var $ul =  $("<ul></ul>");
@@ -158,11 +155,9 @@ $.fn.fieldsLinker = function(action,input) {
 					.attr("data-offset",i)
 					.attr("data-name",x)
 					.text(x);
-				
 			});
 
 
-			
 			var $ul =  $("<ul></ul>");
 			$ul
 				.appendTo($rightDiv)
@@ -176,9 +171,9 @@ $.fn.fieldsLinker = function(action,input) {
 					.attr("data-offset",i)
 					.attr("data-name",x)
 					.text(x);
-				
 			});
 			
+
 			
 			canvasId = "cnv_"+Date.now();
 			
@@ -187,35 +182,66 @@ $.fn.fieldsLinker = function(action,input) {
 			var h1 = $leftDiv.height();	
 			var h = h1 >= h2 ? h1 : h2;
 			var $canvas =  $("<canvas></canvas>");
+			
 			$canvas
 				.appendTo($midDiv)
 				.attr("id",canvasId)
 				.css({"width": w+"px","height":h+"px"});
 			
-			//canvasWidth = w;
-			canvasHeight = h;				
+			canvasWidth = w;
+			canvasHeight = h;		
+            canvasHalfWidth = Math.floor(0.5 + w/2);			
 			canvasPtr= document.getElementById(canvasId);
 			canvasPtr.width = canvasWidth;
 			canvasPtr.height = canvasHeight;
 			canvasCtx = canvasPtr.getContext("2d");
 			
+			// Computing the vertical offset of the middle of each cell.
+			$(this).find(".FL-main .FL-left li").each(function(i){
+				var position = $(this).position();
+				var hInner = $(this).height();
+				var hOuter = $(this).outerHeight();
+				var delta = Math.floor(0.5 + (hOuter - hInner)/2);
+				var midInner = Math.floor(0.5 + hInner/2);
+				var midHeight = position.top + midInner - delta;
+				ListHeights1.push(midHeight);
+			});
+			
+			// Computing the vertical offset of the middle of each cell.
+			$(this).find(".FL-main .FL-right li").each(function(i){
+				var position = $(this).position();
+				var hInner = $(this).height();
+				var hOuter = $(this).outerHeight();
+				var delta = Math.floor(0.5 + (hOuter - hInner)/2);
+				var midInner = Math.floor(0.5 + hInner/2);
+				var midHeight = position.top + midInner - delta;
+				ListHeights2.push(midHeight);
+			});
+			
+			// Listeners :
+			
+			// On mousedown in left List : 
 			$(this).find(".FL-main .FL-left li").on("mousedown",function(e){
-				move = {};
+				// we make a move object to keep track of the origine and also remember that we are starting a mouse drag (mouse is down)
+				 move = {};
 				 move.offsetA = $(this).data("offset");
 				 move.nameA = $(this).data("name");
 				 move.offsetB = -1;
 				 move.nameB = -1;
-			});
-			
-			$(this).find(".FL-main .FL-left li").on("mouseup",function(e){
-				 move=null;
+				 // If the link already exists then we erase it
 				 eraseLinkA($(this).data("offset"));
 			});
 			
+			$(this).find(".FL-main .FL-left li").on("mouseup",function(e){
+				// We do a mouse up on le teft side : the drag is canceled
+				 move=null;
+			});
+			
+			// Mouse up on the right side 
 			$(this).find(".FL-main .FL-right li").on("mouseup",function(e){
-				 if(move == null){
-					 eraseLinkB($(this).data("offset"));
-				 }else{
+				 if(move == null){ // no drag 
+					 eraseLinkB($(this).data("offset")); // we erase an existing link if any
+				 }else{ // we finish a drag then get the infos an create a link
 					 move.offsetB = $(this).data("offset");
 					 move.nameB = $(this).data("name");
 					 var infos =  JSON.parse(JSON.stringify(move));
@@ -224,10 +250,63 @@ $.fn.fieldsLinker = function(action,input) {
 				 }
 			});
 			
+			// mousemove over a right cell
+			$(this).find(".FL-main .FL-right li").on("mousemove",function(e){
+				 if(move != null){ // drag occuring
+		
+					 var _from = move.offsetA;
+					 var _To = $(this).data("offset");
+					 
+					 var Ax = 0;
+					 var Ay = ListHeights1[_from];
+					 
+					 var Bx = canvasWidth;
+					 var By = ListHeights2[_To];
+					 
+					draw();
+					
+					canvasCtx.moveTo(Ax, Ay);
+					canvasCtx.lineTo(Bx, By);
+					canvasCtx.stroke();
+				 }
+			});
+			
+			// mousemove over the canvas
+			
+			$(this).find("canvas").on("mousemove",function(e){
+				if(move != null){
+					// we redraw all the existing links
+					draw();
+					// we draw the new would-be link
+				
+					var _from = move.offsetA;
+					
+					var Ax = 0;
+					var Ay = ListHeights1[_from];
+					// mouse position relative to the canvas
+					var Bx = e.offsetX;
+					var By = e.offsetY;
+					
+					canvasCtx.moveTo(Ax, Ay);
+					canvasCtx.lineTo(Bx, By);
+					canvasCtx.stroke();
+					
+				}
+			});
+			
+			$(this).find(".FL-main").on("mouseup",function(e){
+				if(move!=null){
+					move = null;
+					draw();
+				}
+					
+			});
+			
+			
+			
 			data.existingLinks.forEach(function(link){
 					var pos = -1;
 					if(byName){
-						
 						var offA = List1.indexOf(link["from"]);
 						var offB = List2.indexOf(link["to"]);
 						if(offA !=-1 && offB!=-1){
@@ -249,9 +328,7 @@ $.fn.fieldsLinker = function(action,input) {
 						linksByOrder.push(link);
 						linksByName.push(linkWithNames);				
 						}
-
 					}
-
 			});
 			
 			that = this; // keep the context for listeners
